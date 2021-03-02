1fa6ec6d0bd205966678afef31beb8ead2ffa6ca
Ken Gilmer <kgilmer@gmail.com>
2021-03-02 12:03:09 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # This script is intended to be run upon creation of new VM to populate repo
+set -x
 
 sudo add-apt-repository -y ppa:videolan/master-daily # necessary for debhelper-compat 13 required by json-glib
 sudo apt install devscripts jq wget dpkg-dev coreutils curl gnupg reprepro debhelper
 
 if [ ! -d docs ]; then
   mkdir -p docs/conf
-  cp distributions-buster repo/conf/distributions
+  cp distributions-buster docs/conf/distributions
 fi
